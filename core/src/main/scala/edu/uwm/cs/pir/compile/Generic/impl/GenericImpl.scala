4574d5f6b81bd173e5d7d74938ff9abf5322ad93
pir-dsl <matthewhuang2004@gmaill.com>
2014-04-27 10:46:54 -0500

    Added temp change

--- core/src/main/scala/edu/uwm/cs/pir/compile/Generic/impl/GenericImpl.scala
+++ core/src/main/scala/edu/uwm/cs/pir/compile/Generic/impl/GenericImpl.scala
@@ -201,14 +201,7 @@ object GenericImpl {
         }
       }
       val resultList = accums.map(d => InvertedIndexSearchResult(d._1, d._2.docStringId, index.dataset(d._1), d._2.score / docNorm(d._1))).toList.filter(elem => !elem.score.isNaN())
-      try {
-        //resultList.sorted.take(topk)
-        resultList.sortWith(_>_).take(topk)
-      } catch {
-        case e: Exception =>
-          resultList.foreach(elem => elem.printResult)
-          throw e
-      }
+        resultList.sorted.take(topk)   
     }
   }
 
