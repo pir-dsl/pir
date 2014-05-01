fadfd13c8d5278988b92d49b8e302888924c8e99
pir-dsl <matthewhuang2004@gmaill.com>
2014-05-01 00:01:36 -0500

    Added temp change

--- core/src/main/scala/edu/uwm/cs/pir/strategy/Strategy.scala
+++ core/src/main/scala/edu/uwm/cs/pir/strategy/Strategy.scala
@@ -240,7 +240,7 @@ object Strategy {
     v.visitedPath
   }
   
-  def getPersistedId (vp: String) = vp.substring(vp.indexOf("<<<"), vp.indexOf(">>>"))
+  def getPersistedId (vp: String) = {log(vp)("INFO");vp.substring(vp.indexOf("<<<"), vp.indexOf(">>>"))}
   
   def checkS3Persisted[In <: IFeature, Index <: IIndex: ClassTag] (source: SourceComponent[In], S3Location : String) : Boolean  = {
     val vp = getVisitedPath(source)
