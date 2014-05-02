d4d5794823f34040f5483cf995e044af868a0f51
pir-dsl <matthewhuang2004@gmaill.com>
2014-05-01 20:41:32 -0500

    Added temp change

--- core/src/main/scala/edu/uwm/cs/pir/compile/Visitor.scala
+++ core/src/main/scala/edu/uwm/cs/pir/compile/Visitor.scala
@@ -32,24 +32,24 @@ class JobVisitor extends Visitor {
   var visitedPath: String = ""
 
   override def visit[In <: IFeature, Out <: IFeature: ClassTag](load: LoadStage[In, Out]) {
-    if (!queue.contains(load)) queue.enqueue((load.getClass.getSimpleName, load))
+    if (!queue.contains(load)) queue.enqueue((load.getClass.getSimpleName + "->" + load.load.getClass.getSimpleName, load))
     visitedPath += load.load.getInfo
   }
 
   override def visit[In <: IFeature, Out <: IFeature: ClassTag](pipe: SourcePipe[In, Out]) {
     pipe.left.accept(this)
     pipe.right.accept(this)
-    if (!queue.contains(pipe)) queue.enqueue((pipe.getClass.getSimpleName, pipe))
+    if (!queue.contains(pipe)) queue.enqueue((pipe.getClass.getSimpleName + "->" + pipe.right.getClass.getSimpleName, pipe))
   }
 
   override def visit[In <: IFeature: ClassTag](pipe: FilterPipe[In]) {
     pipe.left.accept(this)
-    if (!queue.contains(pipe)) queue.enqueue((pipe.getClass.getSimpleName, pipe))
+    if (!queue.contains(pipe)) queue.enqueue((pipe.getClass.getSimpleName + "->" + pipe.right.getClass.getSimpleName, pipe))
   }
 
   override def visit[In <: IFeature: ClassTag](pipe: SortPipe[In], order: Boolean) {
     pipe.left.accept(this)
-    if (!queue.contains(pipe)) queue.enqueue((pipe.getClass.getSimpleName, pipe))
+    if (!queue.contains(pipe)) queue.enqueue((pipe.getClass.getSimpleName + "->" + "Sort", pipe))
   }
 
   override def visit[In <: IFeature, Middle <: IFeature, Out <: IFeature](pipe: ProjPipe[In, Middle, Out]) {}
@@ -65,34 +65,34 @@ class JobVisitor extends Visitor {
 
   override def visit[In <: IFeature, Model <: IModel](train: TrainStage[In, Model]) {
     train.source.accept(this)
-    if (!queue.contains(train)) queue.enqueue((train.getClass.getSimpleName, train))
+    if (!queue.contains(train)) queue.enqueue((train.getClass.getSimpleName + "->" + train.trainer.getClass.getSimpleName, train))
   }
 
   override def visit[In <: IFeature, In2 <: IFeature, Model <: IModel](train: TrainStage2[In, In2, Model]) {
     train.source1.accept(this)
     train.source2.accept(this)
-    if (!queue.contains(train)) queue.enqueue((train.getClass.getSimpleName, train))
+    if (!queue.contains(train)) queue.enqueue((train.getClass.getSimpleName + "->" + train.trainer.getClass.getSimpleName, train))
   }
 
   override def visit[In <: IFeature, Out <: IFeature, Index <: IIndex](query: InvertedIndexQueryStage[In, Out, Index]) {
     query.index.accept(this)
     query.source.accept(this)
-    if (!queue.contains(query)) queue.enqueue((query.getClass.getSimpleName, query))
+    if (!queue.contains(query)) queue.enqueue((query.getClass.getSimpleName + "->" + query.query.getClass.getSimpleName, query))
   }
 
   override def visit[In <: IFeature, Index <: IIndex: ClassTag](index: HistogramIndexStage[In, Index]) {
     index.source.accept(this)
-    if (!queue.contains(index)) queue.enqueue((index.getClass.getSimpleName, index))
+    if (!queue.contains(index)) queue.enqueue((index.getClass.getSimpleName + "->" + index.indexer.getClass.getSimpleName, index))
   }
 
   override def visit[In <: IFeature, Index <: IIndex](index: IndexStage[In, Index]) {
     index.source.foreach(elem => elem.accept(this))
-    if (!queue.contains(index)) queue.enqueue((index.getClass.getSimpleName, index))
+    if (!queue.contains(index)) queue.enqueue((index.getClass.getSimpleName + "->" + index.indexer.getClass.getSimpleName, index))
   }
 
   override def visit[In <: IFeature, Out <: IFeature, Index <: IIndex, Compose <: ICompose](query: LuceneQueryStage[In, Out, Index, Compose]) {
     query.index.accept(this)
     query.source.accept(this)
-    if (!queue.contains(query)) queue.enqueue((query.getClass.getSimpleName, query))
+    if (!queue.contains(query)) queue.enqueue((query.getClass.getSimpleName + "->" + query.query.getClass.getSimpleName, query))
   }
 }
